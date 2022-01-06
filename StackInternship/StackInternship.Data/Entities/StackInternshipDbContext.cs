a68b7290be31ec295aad21d93c1a2ddcd7228941
matea baturina <mbaturina@pmfst.hr>
2022-01-06 22:38:46 +0100

    Adding Database context

---
+++ StackInternship/StackInternship.Data/Entities/StackInternshipDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Linq;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace StackInternship.Data.Entities
+{
+    public class StackInternshipDbContext : DbContext
+    {
+        public StackInternshipDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //DatabaseSeeder.Execute(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class StackInternshipContextFactory : IDesignTimeDbContextFactory<StackInternshipDbContext>
+    {
+        public StackInternshipDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("App.config")
+                .Build();
+
+            configuration
+                .Providers
+                .First()
+                .TryGet("connectionString:add:StackInternship:connectionString", out var connectionString);
+
+            var options = new DbContextOptionsBuilder<StackInternshipDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            return new StackInternshipDbContext(options);
+        }
+    }
+}
---
+++ StackInternship/StackInternship.Domain/Factories/DbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StackInternship.Data.Entities;
+using System.Configuration;
+
+namespace StackInternship.Domain.Factories
+{
+    public static class DbContextFactory
+    {
+        public static StackInternshipDbContext GetPaymentManagerDbContext()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseSqlServer(ConfigurationManager.ConnectionStrings["StackInetrnship"].ConnectionString)
+                .Options;
+
+            return new StackInternshipDbContext(options);
+        }
+    }
+}
